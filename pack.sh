c3458b9c24275def19d12dcdeadbb8f237f7bc74
tom <tom@tompc.(none)>
2008-12-10 17:15:31 +0200

    removed pack.sh and added a doc dir. moved the query specs for the view to the doc dir

--- pack.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-python setup.py install --root=package
-rm -rf build
-mkdir package/CONTROL
-cp CONTROL/postinst CONTROL/postrm CONTROL/control package/CONTROL/
-/usr/local/openmoko/arm/bin/ipkg-build -o root -g root package/
-rm -rf package
