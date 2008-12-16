95d7cd03c8353602729a8888b61b9509c67c00d7
Tom Hacohen <tom@nospam>
2008-12-16 17:34:54 +0200

    added the tab pixmaps

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import glob
     
 setup(name='pyphonelog',
-        version='0.15.4',
+        version='0.15.5',
 	description='A phonelog gui that connects to the shr daemon/a custom daemon',
         author='Tom Hacohen',
         author_email='tom@stosb.com',
