9dd520a2b5e84aac86d8aafd19062b6398e22064
David Garabana Barro <david@garabana.com>
2009-12-27 16:33:14 +0100

    Add galician and spanish translations Signed-off-by: Sebastian Krzyszkowiak <seba.dos1@gmail.com>

--- setup.py
+++ setup.py
@@ -23,6 +23,8 @@ setup(name='pyphonelog',
 		('pixmaps', ['data/pixmaps/phonelog.png']),
 		('locale/de/LC_MESSAGES', ['data/po/de/phonelog.mo']),
                 ('locale/fr/LC_MESSAGES', ['data/po/fr/phonelog.mo']),
+                ('locale/es/LC_MESSAGES', ['data/po/es/phonelog.mo']),
+                ('locale/gl/LC_MESSAGES', ['data/po/gl/phonelog.mo']),
 		('phonelog/skeleton', ['data/config/phonelog.conf']),
 		('phonelog/icons', ['data/pixmaps/ui/missed.png',\
 				    'data/pixmaps/ui/received.png',\
